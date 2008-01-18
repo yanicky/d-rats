b0b80f0213cde36df6e3ffe2f3a3e5a48d6a614a
Dan Smith <danms@us.ibm.com>
2008-01-18 15:19:09 -0800

    XModemCRC works, and receive auto-detects method

--- xmodem.py
+++ xmodem.py
@@ -50,6 +50,7 @@ class XModemChecksum:
     def __init__(self):
         self.c = 0
         self.r = 0
+        self.title = "XModem"
 
     def process_block(self, data):
         for i in data:
@@ -68,9 +69,13 @@ class XModemChecksum:
         return self.c == self.r
 
     def __str__(self):
-        return "Checksum: 0x%02x,0x%02x" % (self.c, self.r)
+        return "%s Checksum: 0x%02x,0x%02x" % (self.title, self.c, self.r)
 
 class XModemCRCChecksum(XModemChecksum):
+    def __init__(self):
+        XModemChecksum.__init__(self)
+        self.title = "XModemCRC"
+    
     def update_crc(self, c, crc):
         for _ in range(0,8):
             c <<= 1
@@ -105,14 +110,12 @@ class XModemCRCChecksum(XModemChecksum):
         self.r = (ord(data[0]) << 8) | (ord(data[1]))
 
     def write(self, pipe):
-        data = [chr((self.c >> 8) & 0xFF),
-                chr(self.c & 0xFF)]
+        data = chr((self.c >> 8) & 0xFF) + chr(self.c & 0xFF)
         pipe.write(data)
 
 class XModem:
     def __init__(self, debug=None):
         self.block_size = 128
-        self.checksum_size = 1
         self.start_xfer = NAK
         self.data = ""
         if debug:
@@ -122,6 +125,9 @@ class XModem:
                 self._debug = file(debug, "w")
         else:
             self._debug = None
+
+        self.checksums = {"C" : XModemCRCChecksum,
+                          NAK : XModemChecksum}
 
     def debug(self, str):
         if not self._debug:
@@ -153,7 +159,7 @@ class XModem:
         pipe.write(hdr)
 
     def recv_block(self, pipe):
-        csum = XModemChecksum()
+        csum = self.checksum()
         
         try:
             block_num = self.read_header(pipe)
@@ -182,7 +188,7 @@ class XModem:
         self.debug("Sending block %i (%i bytes)" % (num, len(block)))
 
         pipe.write(block)
-        csum = XModemChecksum()
+        csum = self.checksum()
         csum.process_block(block)
         csum.write(pipe)
 
@@ -196,8 +202,9 @@ class XModem:
             raise Exception("NAK on block %i (`%s':%i)" % (num, ack, len(ack)))
 
     def recv_xfer(self, pipe):
+        self.checksum = self.checksums[self.start_xfer]
         pipe.write(self.start_xfer)
-        self.debug("Sent start")
+        self.debug("Sent start: 0x%02x" % ord(self.start_xfer))
 
         data = "aa"
 
@@ -211,8 +218,7 @@ class XModem:
         self.debug("Transfer complete (%i bytes)" % len(self.data))
 
     def send_xfer(self, pipe, source):
-        global debug
-        #pipe.write("Start receive now...\n")
+        pipe.write("Start receive now...\n")
 
         for i in range(0,10):
             self.debug("Waiting for start %i" % time.time())
@@ -241,6 +247,7 @@ class XModem:
         data = "aa"
         blockno = 0
 
+        self.checksum = self.checksums[start]
 
         blocks = len(data) / self.block_size
 
@@ -257,52 +264,12 @@ class XModem:
             self.crc = 0
 
         self.debug("Transfer finished")
-        
 
 class XModemCRC(XModem):
     def __init__(self, debug=None):
-        self.block_size = 128
-        self.checksum_size = 2
+        XModem.__init__(self, debug)
         self.start_xfer = "C"
-        self.crc = 0
-        self.data = ""
-
-        XModem.__init__(self, debug=debug)
-
-    def update_crc(self, c, crc):
-        for _ in range(0,8):
-            c <<= 1
-
-            if (c & 0400) != 0:
-                v = 1
-            else:
-                v = 0
-                
-            if (crc & 0x8000):
-                crc <<= 1
-                crc += v
-                crc ^= 0x1021
-            else:
-                crc <<= 1
-                crc += v
-
-        return crc & 0xFFFF
-
-    def checksum(self, data):
-        for i in data:
-            self.crc = self.update_crc(ord(i) & 0377, self.crc)
-
-        self.crc = self.update_crc(0, self.crc)
-        self.crc = self.update_crc(0, self.crc)
-
-        return self.crc
-
-    def compare_checksum(self, r, c):
-        rcsum = (ord(r[0]) << 8) | ord(r[1])
-
-        self.crc = 0
-
-        return rcsum == c
+        self.debug("XMODEMCRC")
 
 if __name__ == "__main__":
     p = ptyhelper.PtyHelper("python sx.py xmodem.py")
@@ -310,7 +277,7 @@ if __name__ == "__main__":
     #p = ptyhelper.PtyHelper("rx -v outputfile")
     #p = ptyhelper.PtyHelper("sx xmodem.py")
     
-    x = XModem(debug="stdout")
+    x = XModemCRC(debug="stdout")
 
     try:
         # Eat up the buffer
