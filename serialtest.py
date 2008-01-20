4f80cf1078a4616449b164cb5433d45ba9069a2e
Dan Smith <danms@us.ibm.com>
2008-01-19 17:44:27 -0800

    Print portstring in serialtest

--- serialtest.py
+++ serialtest.py
@@ -11,12 +11,14 @@ x = xmodem.XModemCRC(debug="stdout")
 
 if sys.argv[1] == "recv":
     p = serial.Serial(port="/dev/ttyS0", baudrate=115200, timeout=2)
+    print p.portstr
     p.read(200)
     x.recv_xfer(p)
     o = file("output", "w")
     o.write(x.data)
 elif sys.argv[1] == "send":
     p = serial.Serial(port="/dev/ttyUSB0", baudrate=115200, timeout=2)
+    print p.portstr
     p.read(200)
     i = file("xmodem.py")
     x.send_xfer(p, i)
