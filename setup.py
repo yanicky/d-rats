f46433314b207eef3ee8d387a038e51d6621cb86
Dan Smith <danms@us.ibm.com>
2008-02-10 07:10:10 -0800

    Tweaks

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ import py2exe
 
 opts = {
     "py2exe" : {
-    "includes" : "pango,atk,gobject,cairo,pangocairo",
-    }
+        "includes" : "pango,atk,gobject,cairo,pangocairo",
+        "compressed" : 1,
+        "optimize" : 2
+        }
     }
 
-setup(windows=["d-rats.py"], options=opts)
+setup(windows=["d-rats.py"],
+      options=opts)
