bff1fc7d957b130e5db1273242a6a9fc5acbcb86
Dan Smith <danms@us.ibm.com>
2008-02-11 19:44:43 -0800

    Add a simple choice dialog

--- inputdialog.py
+++ inputdialog.py
@@ -2,6 +2,8 @@
 
 import gtk
 
+from config import make_choice
+
 class TextInputDialog(gtk.Dialog):
     def respond_ok(self, entry, data=None):
         self.response(gtk.RESPONSE_OK)
@@ -21,6 +23,21 @@ class TextInputDialog(gtk.Dialog):
 
         self.label.show()
         self.text.show()
+
+class ChoiceDialog(gtk.Dialog):
+    def __init__(self, choices, title=None):
+        buttons = (gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL,
+                   gtk.STOCK_OK, gtk.RESPONSE_OK)
+        gtk.Dialog.__init__(self, title, buttons=buttons)
+
+        self.label = gtk.Label()
+        self.label.set_size_request(300,100)
+        self.vbox.pack_start(self.label, 1, 1, 0)
+        self.label.show()
+
+        self.choice = make_choice(choices, False, choices[0])
+        self.vbox.pack_start(self.choice, 1, 1, 0)
+        self.choice.show()
 
 if __name__ == "__main__":
     d = TextInputDialog("Foo")
