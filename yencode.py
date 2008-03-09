e7a4bf66cccfc1e04b7d6a458a9c0d30880f258f
Dan Smith <danms@us.ibm.com>
2008-03-08 19:20:53 -0800

    Add custom yEnc implementation

---
+++ yencode.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+#
+# Copyright 2008 Dan Smith <dsmith@danplanet.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+DEFAULT_BANNED = "\x021\x023\032"
+
+def yencode_buffer(buf, banned=None):
+    if not banned:
+        banned = DEFAULT_BANNED
+
+    banned += "="
+    out = ""
+        
+    for char in buf:
+        if char in banned:
+            out += "=" + chr((ord(char) + 64) % 256)
+        else:
+            out += char
+
+    return out
+
+def ydecode_buffer(buf):
+    out = ""
+    
+    i = 0
+    while i < len(buf):
+        char = buf[i]
+        if char == "=":
+            i += 1
+            v = ord(buf[i]) - 64
+            if v < 0:
+                print >>sys.stderr, "V is %i" % v
+                v += 255
+            out += chr(v)
+        else:
+            out += char
+
+        i += 1
+
+    return out
+
+if __name__=="__main__":
+    import sys
+
+    f = file(sys.argv[2])
+    inbuf = f.read()
+
+    if sys.argv[1] == "-e":
+        sys.stdout.write(yencode_buffer(inbuf))
+    else:
+        sys.stdout.write(ydecode_buffer(inbuf))
+
+    f.close()
