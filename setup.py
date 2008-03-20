7806b30cfdb930208298183a764d5c8c03a91780
Dan Smith <danms@us.ibm.com>
2008-03-20 16:32:38 -0700

    Setup changes

--- setup.py
+++ setup.py
@@ -6,11 +6,13 @@ opts = {
         "includes" : "pango,atk,gobject,cairo,pangocairo,xml",
         "compressed" : 1,
         "optimize" : 2,
+        "bundle_files" : 3,
 #        "packages" : ""
         }
     }
 
-setup(windows=[{'script' : "d-rats.py",
-               'icon_resources': [(0x0004, 'd-rats.ico')]},
-               {'script' : 'repeater.py'}],
-      options=opts)
+setup(
+    windows=[{'script' : "d-rats.py",
+              'icon_resources': [(0x0004, 'd-rats.ico')]},
+             {'script' : 'repeater.py'}],
+    options=opts)
