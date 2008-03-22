0559088929c9daab6c67685a71b4ae2176831607
Dan Smith <danms@us.ibm.com>
2008-03-22 12:29:51 -0700

    Cleanup of call and comment parsing, start on ability to convert back to NMEA for faking GPS position

--- gps.py
+++ gps.py
@@ -1,4 +1,5 @@
 import re
+import time
 
 TEST = "$GPGGA,180718.02,4531.3740,N,12255.4599,W,1,07,1.4,50.6,M,-21.4,M,,*63 KE7JSS  ,440.350+ PL127.3"
 
@@ -18,14 +19,26 @@ class GPSPosition:
                                         int(t[2:4]),
                                         int(t[4:6]))
 
-        self.latitude = "%s %s" % (m.group(2), m.group(3))
-        self.longitude = "%s %s" % (m.group(4), m.group(5))
+        if m.group(3) == "S":
+            mult = -1
+        else:
+            mult = 1
+        self.latitude = float(m.group(2)) * mult
+
+        if m.group(5) == "W":
+            mult = -1
+        else:
+            mult = 1
+        self.longitude = float(m.group(4)) * mult
+
         self.satellites = int(m.group(7))
         self.altitude = "%s %s" % (m.group(9), m.group(10))
         self.station = m.group(13).split(' ', 1)[1].strip()
-        self.comment = m.group(14)
+        self.comment = m.group(14).strip()
         
     def __init__(self, string):
+        string = string.replace('\r', ' ')
+        string = string.replace('\n', ' ') 
         try:
             self.parse_string(string)
             self.valid = True
@@ -45,6 +58,21 @@ class GPSPosition:
         else:
             return "GPS: (Invalid GPS data)"
 
+    def to_NMEA_GGA(self, station, comment="D-RATS"):
+        date = time.strftime("%H%M%S")
+        if self.latitude > 0:
+            lat = "%.4f,%s" % (self.latitude, "N")
+        else:
+            lat = "%.4f,%s" % (self.latitude * -1, "S")
+
+        if self.longitude > 0:
+            lon = "%.4f,%s" % (self.longitude, "E")
+        else:
+            lon = "%.4f,%s" % (self.longitude * -1, "W")
+
+        return "$GPGGA,%s,%s,%s,1,0,0,0,M,0,M,,*00 %s,%s" % ( \
+            date, lat, lon, station, comment)
+
 def parse_GPS(string):
     if string.startswith("$GPGGA,"):
         return GPSPosition(string)
@@ -63,3 +91,4 @@ if __name__ == "__main__":
         print "Comment:    %s" % p.comment
 
         print "\n%s" % str(p)
+        print "\n%s" % p.to_NMEA_GGA("KI4IFW")
