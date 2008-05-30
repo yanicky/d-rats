ed20fe8fad4be2bbbc3d0bdb3d73a4e1d061054d
Dan Smith <dsmith@danplanet.com>
2008-05-29 21:00:16 -0700

    Make transport layer notice and parse GPS beacons and stuff them into chat packets.

--- transport.py
+++ transport.py
@@ -87,6 +87,12 @@ class Transporter:
             else:
                 self.inbuf += chunk
 
+    def _handle_frame(self, frame):
+        if self.inhandler:
+            self.inhandler(frame)
+        else:
+            self.inq.enqueue(frame)
+
     def parse_blocks(self):
         while ddt2.ENCODED_HEADER in self.inbuf and \
                 ddt2.ENCODED_TRAILER in self.inbuf:
@@ -94,18 +100,55 @@ class Transporter:
             e = self.inbuf.index(ddt2.ENCODED_TRAILER) + \
                 len(ddt2.ENCODED_TRAILER)
 
+            if e < s:
+                # Excise the extraneous end
+                _tmp = self.inbuf[:e-len(ddt2.ENCODED_TRAILER)] + \
+                    self.inbuf[e:]
+                self.inbuf = _tmp
+                continue
+
             block = self.inbuf[s:e]
-            self.inbuf = self.inbuf[e:]
+            _tmp = self.inbuf[:s] + self.inbuf[e:]
+            self.inbuf = _tmp
 
             f = ddt2.DDT2EncodedFrame()
-            if f.unpack(block):
-                print "Got a block: %s" % f
-                if self.inhandler:
-                    self.inhandler(f)
+            try:
+                if f.unpack(block):
+                    print "Got a block: %s" % f
+                    self._handle_frame(f)
                 else:
-                    self.inq.enqueue(f)
-            else:
-                print "Found a broken block"
+                    print "Found a broken block"
+            except Exception, e:
+                print "Failed to unpack what looked like a block"
+
+    def _match_gps(self, type):
+        return re.match("^(.*)(%s,.*\r\n.*\r)(.*)" % type, self.inbuf)
+
+    def _send_text_block(self, string):
+        f = ddt2.DDT2EncodedFrame()
+        f.seq = 0
+        f.session = 1 # Chat (for now)
+        f.s_station = "CQCQCQ"
+        f.d_station = "CQCQCQ"
+        f.data = string
+        
+        self._handle_frame(f)
+
+    def _parse_gps(self, type):
+        gga = self._match_gps(type)
+        if gga:
+            self.inbuf = gga.group(1) + gga.group(3)
+            print "Found GPS string: %s" % gga.group(2)
+            self._send_text_block(gga.group(2))
+        else:
+            return None
+
+    def parse_gps(self):
+        types = ["\$GPGGA", "\$GPRMC", "\$CRC[A-z0-9]{4}"]
+
+        for t in types:
+            while self._match_gps(t):
+                self._parse_gps(t)
 
     def send_frames(self):
         while True:
@@ -121,6 +164,10 @@ class Transporter:
         while self.enabled:
             self.get_input()
             self.parse_blocks()
+            self.parse_gps()
+            if self.inbuf:
+                print "### Unconverted data: %s" % self.inbuf
+            self.inbuf = ""
             self.send_frames()
 
     def disable(self):
@@ -149,8 +196,12 @@ class TestPipe:
             self.buf += "asg;sajd;jsadnkbasdl;b  as;jhd[SOB]laskjhd" + \
                 "asdkjh[EOB]a;klsd" + f.get_packed() + "asdljhasd[EOB]" + \
                 "asdljb  alsjdljn[asdl;jhas"
+            
+            if i == 5:
+                self.buf += "$GPGGA,075519,4531.254,N,12259.400,W,1,3,0,0.0,M,0,M,,*55\r\nK7HIO   ,GPS Info\r"
 
-        print "Made some data"
+
+        print "Made some data: %s" % self.buf
 
     
     def __init__(self, src="Sender", dst="Recvr"):
@@ -185,7 +236,7 @@ def test_simple():
 
     time.sleep(2)
 
-    f = t.recv_block()
+    f = t.recv_frame()
     print "Received block: %s" % f
 
     t.disable()
