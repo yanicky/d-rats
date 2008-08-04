fb3f55b127d363b5b239455d6094425982bd5936
Dan Smith <dsmith@danplanet.com>
2008-08-04 15:54:00 -0700

    Further fix transport GPS parsing

--- transport.py
+++ transport.py
@@ -122,14 +122,20 @@ class Transporter:
             except Exception, e:
                 print "Failed to unpack what looked like a block: %s" % e
 
-    def _match_gps(self, type):
+    def _match_gps(self):
         # NMEA-style
-        m = re.match("^(.*)(%s,.*\r\n[^\r]*\r){1,4}([^\$].*)" % type, self.inbuf)
+        m = re.match("^(.*)((\$GP[A-Z]{3},[^\r]*\r\n?){1,4}([^\r]*\r))(.*)", self.inbuf)
         if m:
-            return m
-
+            g = m.groups()
+            return g[0], g[1], g[-1]
+ 
         # GPS-A style
-        return re.match("^(.*)(%s,[^\r]*\r\n)(.*)" % type, self.inbuf)
+        m = re.match("^(.*)(\$\$CRC[A-z0-9]{4},[^\r]*\r\n)(.*)", self.inbuf)
+        if m:
+            g = m.groups()
+            return g[0], g[1], g[2]
+
+        return None
 
     def _send_text_block(self, string):
         f = ddt2.DDT2EncodedFrame()
@@ -141,22 +147,19 @@ class Transporter:
         
         self._handle_frame(f)
 
-    def _parse_gps(self, type):
-        gga = self._match_gps(type)
-        if gga:
-            self.inbuf = gga.group(1) + gga.group(3)
-            print "Found GPS string: %s" % gga.group(2)
-            self._send_text_block(gga.group(2))
+    def _parse_gps(self):
+        result = self._match_gps()
+        if result:
+            self.inbuf = result[0] + result[2]
+            print "Found GPS string: %s" % {"" : result[1]}
+            self._send_text_block(result[1])
         else:
             return None
 
     def parse_gps(self):
-        types = ["\$GPGGA", "\$GPRMC", "\$\$CRC[A-z0-9]{4}"]
-
-        for t in types:
-            while self._match_gps(t):
-                self._parse_gps(t)
-
+        while self._match_gps():
+            self._parse_gps()
+            
     def send_frames(self):
         while True:
             f = self.outq.dequeue()
@@ -215,9 +218,7 @@ class TestPipe:
 
             elif i == 2:
                 self.buf += \
-"""$GPGGA,023531.36,4531.4940,N,12254.9766,W,1,07,1.3,63.7,M,-21.4,M,,*64       
-$GPRMC,023531.36,A,4531.4940,N,12254.9766,W,0.00,113.7,010808,17.4,E,A*27    
-K7TAY M ,/10-13/"""
+"""$GPGGA,023531.36,4531.4940,N,12254.9766,W,1,07,1.3,63.7,M,-21.4,M,,*64\r\n$GPRMC,023531.36,A,4531.4940,N,12254.9766,W,0.00,113.7,010808,17.4,E,A*27\rK7TAY M ,/10-13/\r"""
                 
 
         print "Made some data: %s" % self.buf
